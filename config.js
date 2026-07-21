18fea2d7ffbe461f58382c7f5ca1bf0b7838e777
zsoz11111-hub <zsoz11111@gmail.com>
2026-07-22 01:29:01 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://qrcfwedbmlsrhgmgwqya.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "https://qrcfwedbmlsrhgmgwqya.supabase.co/rest/v1/"
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_QxUpj14szej9Pt6eOm-rNA_gDMCr36p"
 };
